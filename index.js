1fc5278f1b761db99a86c5f7eff450e3c9fba8e0
rukia <ruqayashar1@gmail.com>
2023-03-31 13:59:58 +0300

    palindrome

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
 }
 
 /* 
   Add your pseudocode here
 */
-
+// write a function called  isPalindrome that takes string as Parameter
+//  use if to check if the string is Palindrome.
+// if the string backward and forward is the same return true
+// else return false.
 /*
   Add written explanation of your solution here
-*/
+*/you create a function that receives a word.we use if statement to determine wether the word entered is equal to module.if true we return true else we return false.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -20,6 +24,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("bro"));
 }
 
 module.exports = isPalindrome;
