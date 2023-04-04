28423d8f0642d5cde526ece83f6351118e4d0579
rukia <ruqayashar1@gmail.com>
2023-04-04 10:33:34 +0300

    completed

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-function isPalindrome(word) {
+ function isPalindrome(word) {
   // Write your algorithm here
-  
+  // const wordArray=["abba","a","racecar","robot","ab"]
+//   if (word===isPalindrome){
+//     return true
+//   }
+// else {
+//   return false
+//   }
+// }
+  const reversedWord = reverse(word);
+  if (word === reversedWord) {
+    return true
+  }
+  else
+  {
+  return false;
+ }
+
+function reverse(word){
+  const wordArray= word.split("");
+  const reversedWordArray=wordArray.reverse();
+  const reversedWord=reversedWordArray.join("")
+  return reversedWord;
+
 }
 
 /* 
@@ -10,26 +32,37 @@ function isPalindrome(word) {
 //  use if to check if the string is Palindrome.
 // if the string backward and forward is the same return true
 // else return false.
-/*
-  Add written explanation of your solution here
-*/you create a function that receives a word.we use if statement to determine wether the word entered is equal to module.if true we return true else we return false.
+
+  // Add written explanation of your solution here
+// you create a function that receives a word.we use if statement to determine wether the word entered is equal to module.if true we return true else we return false.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("mom"));
+  console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("bro"));
+  console.log("=>", isPalindrome("ab"));
+
+  console.log("");
+}
 }
 
-module.exports = isPalindrome;
+ module.exports = isPalindrome;
